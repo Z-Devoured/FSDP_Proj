62694f6fb6ef63cd9a8da6c355f33c839e8d10c8
Z-Devoured <rykellay@hotmail.com>
2023-08-02 11:39:36 +0800

    ha

--- learning-app/client/src/pages/Homepage.jsx
+++ learning-app/client/src/pages/Homepage.jsx
@@ -4,7 +4,7 @@ import { Box, Typography, Button } from '@mui/material';
 function Homepage() {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
-            <img src="src/images/ss.png" alt="React Image" />
+            <img src="/images/ss.png" alt="React Image" />
             <div>
                 <Typography variant="h5" sx={{ my: 2 }}>
                     Rent a station near you 
@@ -13,7 +13,7 @@ function Homepage() {
                     Anytime, anywhere with just 3 simple steps
                 </Typography>
             </div>
-            <img src="src/images/s.png" alt="React Image" />
+            <img src="/images/s.png" alt="React Image" />
             <hr style={{ width: '100%', margin: '2rem 0' }} />
             <Typography variant="subtitle1" sx={{ my: 2 }}>
                 Create your account and start booking now
